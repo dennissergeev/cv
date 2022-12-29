97c88a7ff9ec0364b01fbfd6f1cdf7af2c535916
Denis Sergeev <dennis.sergeev@gmail.com>
2022-12-29 15:14:04 +0000

    fix formatting

--- scripts/format_publications.py
+++ scripts/format_publications.py
@@ -14,7 +14,7 @@ JOURNAL_ABBR = {
     "Journal of Climate": "J. Cli.",
     "Monthly Notices of the Royal Astronomical Society": "MNRAS",
     "Monthly Weather Review": "Mon. Wea. Rev.",
-    "Quarterly Journal of the Royal Meteorological Society": "Q. J. R. Meteorol. Soc.",
+    "Quarterly Journal of the Royal Meteorological Society": "Q. J. R. Meteorol. Soc.",  # noqa
     "The Astrophysical Journal": "ApJ",
     "The Planetary Science Journal": "Planet. Sci. J.",
 }
@@ -23,7 +23,7 @@ JOURNAL_ABBR = {
 def format_publication(pub, my_name):
     """Load publications from a JSON file and format them in TeX."""
     entry = ""
-    
+
     # Format the number of citations
     cit_count = pub["citation_count"]
     if cit_count == 0:
@@ -31,19 +31,19 @@ def format_publication(pub, my_name):
     else:
         cites = cit_count
     if pub["doctype"] == "article":
-        entry += fr"\item[\small{{\textbf{{{cites}}}}}] "
+        entry += rf"\item[\small{{\textbf{{{cites}}}}}] "
     else:
-        entry += fr"\item[\small{{{cites}}}] "
-    
+        entry += rf"\item[\small{{{cites}}}] "
+
     # Format the author list
     authors = []
     my_pos = -1
     for n_auth, author in enumerate(pub["author"]):
         last, first = author.split(", ")
-        first_fmt = ' '.join([f"{i[0]}." for i in first.split(' ')])
+        first_fmt = " ".join([f"{i[0]}." for i in first.split(" ")])
         if my_name in author:
             my_pos = n_auth
-            name = fr"\textbf{{{last}, {first_fmt}}}"
+            name = rf"\textbf{{{last}, {first_fmt}}}"
         else:
             name = f"{last}, {first_fmt}"
         authors.append(name)
@@ -56,38 +56,39 @@ def format_publication(pub, my_name):
             authors_fmt += f" (incl. {authors[my_pos]})"
     elif n_auth > 0:
         authors_fmt += ", ".join(authors[:-1])
-        authors_fmt += fr", \& {authors[-1]}"
+        authors_fmt += rf", \& {authors[-1]}"
     else:
         authors_fmt += {authors[0]}
-        
+
     entry += authors_fmt
-    
+
     # Format the year
     entry += f", {pub['year']}"
-    
+
     # Format the title and URL
     title = pub["title"][0]
-    if (doi := pub.get("doi")):
-        link = fr"https://doi.org/{doi[0]}"
-    elif (url := pub.get("url")):
+    if doi := pub.get("doi"):
+        link = rf"https://doi.org/{doi[0]}"
+    elif url := pub.get("url"):
         link = url
-    elif (page := pub.get("page")):
+    elif page := pub.get("page"):
         if "arxiv" in page[0].lower():
             arxiv_id = page[0].lstrip("arXiv:")
-            link = fr"https://arxiv.org/abs/{arxiv_id}"
+            link = rf"https://arxiv.org/abs/{arxiv_id}"
     else:
-        link = fr"https://ui.adsabs.harvard.edu/abs/{pub['bibcode']}"
+        link = rf"https://ui.adsabs.harvard.edu/abs/{pub['bibcode']}"
 
-    entry += fr", \href{{{link}}}{{{title}}}"
-    
+    entry += rf", \href{{{link}}}{{{title}}}"
+
     # Format the journal
-    if (journal:=pub.get("pub")):
+    if journal := pub.get("pub"):
         if "arxiv" in journal.lower():
             entry += f", {pub['page'][0]}"
         else:
             entry += f", {JOURNAL_ABBR.get(journal, journal)}"
-    
+
     return entry
+
 
 if __name__ == "__main__":
     last_name = "Sergeev"
