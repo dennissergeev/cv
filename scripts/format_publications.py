3ffd93eb310a0265dd78e3f10d075d6a827cbc27
Denis Sergeev <dennis.sergeev@gmail.com>
2024-08-07 10:10:44 +0100

    add a link to the PL article

--- scripts/format_publications.py
+++ scripts/format_publications.py
@@ -115,20 +115,15 @@ if __name__ == "__main__":
     last_name = "Sergeev"
     select_bibcodes = [
         "2024MNRAS.529.1776Z",
-        "2024MNRAS.529.3971M",
         "2024arXiv240219277S",
         "2023JGRD..12839343M",
         "2023GMD....16.5601S",
         "2023GMD....16..621M",
-        "2022MNRAS.517.2383B",
         "2022PSJ.....3..213F",
         "2022PSJ.....3..211T",
         "2022PSJ.....3..212S",
         "2022PSJ.....3..214S",
-        "2022ApJ...930..152C",
-        "2020A&A...639A..99E",
         "2020ApJ...894...84S",
-        "2020ApJ...892L..33J",
     ]
 
     with publications_file.open("r") as f_json:
