c8ff315d842ff707a1fa5c743a8c3e579bb4a3ae
Denis Sergeev <dennis.sergeev@gmail.com>
2025-06-03 09:23:35 +0100

    Refactor some cv sections

--- scripts/format_publications.py
+++ scripts/format_publications.py
@@ -32,7 +32,7 @@ def replace_subscripts_and_superscripts(text):
     return pattern.sub(replacement, text)
 
 
-def format_publication(pub, idx, my_name):
+def format_publication(pub: dict, idx: int = None, my_name: str = None) -> str:
     """Load publications from a JSON file and format them in TeX."""
     # Load journal abbreviations
     with journal_abbr_file.open("r") as f_json:
@@ -70,9 +70,19 @@ def format_publication(pub, idx, my_name):
     # Format the year
     entry += f", {pub['year']}"
 
-    # Format the title and URL
+    # Format the title
     title = pub["title"][0]
     title = replace_subscripts_and_superscripts(title)
+    entry += f", {title}"
+
+    # Format the journal
+    if journal := pub.get("pub"):
+        if "arxiv" in journal.lower():
+            entry += f", {pub['page'][0]}"
+        else:
+            entry += f", {JOURNAL_ABBR.get(journal, journal)}"
+
+    # Add URL
     if doi := pub.get("doi"):
         link = rf"https://doi.org/{doi[0]}"
     elif url := pub.get("url"):
@@ -83,15 +93,7 @@ def format_publication(pub, idx, my_name):
             link = rf"https://arxiv.org/abs/{arxiv_id}"
     else:
         link = rf"https://ui.adsabs.harvard.edu/abs/{pub['bibcode']}"
-
-    entry += rf", \href{{{link}}}{{{title}}}"
-
-    # Format the journal
-    if journal := pub.get("pub"):
-        if "arxiv" in journal.lower():
-            entry += f", {pub['page'][0]}"
-        else:
-            entry += f", {JOURNAL_ABBR.get(journal, journal)}"
+    entry += rf"~\href{{{link}}}{{\link}}"
 
     # Format the number of citations
     cit_count = pub.get("citation_count", 0)
@@ -99,6 +101,9 @@ def format_publication(pub, idx, my_name):
         cites = r"\textbullet"
     else:
         cites = cit_count
+
+    if idx is None:
+        idx = r"\textbullet"
 
     if refereed:
         line = rf"\small{{\highlightdark{{{idx}}}}} & {entry} & \small{{\highlightdark{{\textbf{{{cites}}}}}}} \\"
@@ -110,9 +115,22 @@ def format_publication(pub, idx, my_name):
 
 if __name__ == "__main__":
     last_name = "Sergeev"
+    # Load the publications from the JSON file
+    with publications_file.open("r") as f_json:
+        pubs = json.load(f_json)["docs"]
+    pubs = sorted(pubs, key=lambda x: x["pubdate"], reverse=True)
+
+    # All publications
+    with pubs_formatted.open("w") as f_tex:
+        n_pub = len(pubs)
+        for idx, pub in enumerate(pubs):
+            f_tex.write(format_publication(pub, n_pub - idx, my_name=last_name) + "\n")
+
+    # Publications for the short CV
     select_bibcodes = [
+        "2025arXiv250419883S",
         "2024MNRAS.529.1776Z",
-        "2024arXiv240219277S",
+        "2024ApJ...970....7S",
         "2023JGRD..12839343M",
         "2023GMD....16.5601S",
         "2023GMD....16..621M",
@@ -122,18 +140,6 @@ if __name__ == "__main__":
         "2022PSJ.....3..214S",
         "2020ApJ...894...84S",
     ]
-
-    with publications_file.open("r") as f_json:
-        pubs = json.load(f_json)["docs"]
-    pubs = sorted(pubs, key=lambda x: x["pubdate"], reverse=True)
-    n_pub = len(pubs)
-
-    with pubs_formatted.open("w") as f_tex:
-        for idx, pub in enumerate(pubs):
-            f_tex.write(format_publication(pub, n_pub - idx, my_name=last_name) + "\n")
     with pubs_formatted_short.open("w") as f_tex:
-        for idx, pub in enumerate(pubs):
-            if pub["bibcode"] in select_bibcodes:
-                f_tex.write(
-                    format_publication(pub, n_pub - idx, my_name=last_name) + "\n"
-                )
+        for idx, pub in enumerate([p for p in pubs if p["bibcode"] in select_bibcodes]):
+            f_tex.write(format_publication(pub, my_name=last_name) + "\n")
